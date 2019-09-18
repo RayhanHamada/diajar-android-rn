9ed84bd01dd955eb8ace2f95e7f641e64b6c4131
RayhanHamada <hamadadesk1999@gmail.com>
2019-09-18 23:38:35 +0700

    finish login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,19 +1,77 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Image} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Image,
+  KeyboardAvoidingView,
+  ListView,
+  ScrollView,
+  TextIn,
+} from 'react-native';
 
-import {Text, Input} from 'react-native-elements';
+import {Text, Input, Button} from 'react-native-elements';
+import {Icon} from 'react-native-elements';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 export default LoginPage = props => {
+  const [loginState, setLoginState] = useState({
+    userEmail: '',
+    userPassword: '',
+  });
+
+  const emailInputRef = React.createRef();
+  const passInputRef = React.createRef();
+
   return (
-    <View style={styles.mainContainer}>
-      <Image
-        source={require('./../assets/logo1.png')}
-        style={{width: 100, height: 100}}
-      />
-      <View style={styles.inputs}>
-        <Input placeholder="Enter Your Text" placeholderTextColor="white" />
+    <KeyboardAvoidingView style={{flex: 1}} behavior="padding" keyboardVerticalOffset={-220}>
+      <View style={styles.mainContainer}>
+        <Image
+          source={require('./../assets/logo1.png')}
+          style={{width: 120, height: 120}}
+          resizeMode="center"
+        />
+        <View style={styles.inputs}>
+          <Input
+            ref={emailInputRef}
+            placeholder="Masukkan email anda"
+            placeholderTextColor="#a6aba6"
+            inputStyle={{color: 'white'}}
+            leftIcon={
+              <Icon
+                type="material-community"
+                name="account-outline"
+                color="white"
+              />
+            }
+            onChangeText={val => {
+              setLoginState({userEmail: val});
+            }}
+          />
+          <Input
+            ref={passInputRef}
+            placeholder="Masukkan password anda"
+            placeholderTextColor="#a6aba6"
+            inputStyle={{color: 'white'}}
+            leftIcon={
+              <Icon
+                type="material-community"
+                name="lock-outline"
+                color="white"
+              />
+            }
+            onChangeText={val => {
+              setLoginState({userPassword: val});
+            }}
+          />
+          <Button
+            type="outline"
+            title="Login"
+            containerStyle={styles.loginContainer}
+            buttonStyle={styles.loginButton}
+          />
+        </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -22,11 +80,20 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     backgroundColor: '#002663',
+    flex: 1,
     alignItems: 'center',
-    paddingTop: '20%',
+    paddingTop: '30%',
   },
   inputs: {
     marginTop: '15%',
-    width: '86%',
+    width: '95%',
+    flex: 1,
+    // height: '40%',
+  },
+  loginContainer: {
+    marginTop: '5%',
+  },
+  loginButton: {
+    borderRadius: 20,
   },
 });
